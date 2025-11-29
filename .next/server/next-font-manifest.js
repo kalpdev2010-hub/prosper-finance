efcdffaf421850bd1843feaeff4076852b2ca58c
kalpdev2010-hub <kalpdev2010@gmail.com>
2025-11-29 18:15:49 +0400

    Fix build

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/kalpdev2010-hub/prosper-finance/app/layout\":[\"static/media/6c596dfcddeca1e9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HsPz5ckbrKbn6i7xDGWp0sLqZLuXSchxwPjRbC62TBw=\"\n}"
